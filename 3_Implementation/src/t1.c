30aba62dbce5c2953dadab726182f43ebc4e85a7
aniketmoon5 <94234616+aniketmoon5@users.noreply.github.com>
2021-12-02 09:49:29 +0530

    Update and rename t to t1.c

---
+++ 3_Implementation/src/t1.c
@@ -0,0 +1,34 @@
+
+#include "t1.h"
+
+void peripheral_init(void)
+{	
+	DDRD |= (1<<PD2); // set PD2=1 for LED
+    DDRD &= ~(1<<PD0); //clear bit
+    PORTD |= (1<<PD0); //set bit PD0 for SeatSwitch
+    DDRD &= ~(1<<PD1); //clear bit
+    PORTD |= (1<<PD1); //set bit PD0 for HeaterSwitch
+}
+
+void TurnLED_ON(){
+    LED_PORT |= (1<<LED_PIN); 
+}
+
+void TurnLED_OFF(){
+    LED_PORT &= ~(1<<LED_PIN);
+}
+
+int act1=0;
+int t1_LED(void)
+{
+       peripheral_init();
+        if(!(PIND&(1<<BUTTON_SENSOR )) && !(PIND&(1<<TEMP_SENSOR))) //both the switches are pressed
+        { 
+            act1=1;
+        }
+        else  //in all other cases
+        {
+            act1=0;
+        }
+    return act1;
+}
